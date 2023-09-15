61b3c3340c5f7155f262509b9479c7c57ebbdd81
nubisub <hendraksm18@mail.ugm.ac.id>
2023-09-15 22:52:48 +0700

    add dashboard homepage
    add dashboard homepage with card and nivo bar chart

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,32 +1,205 @@
 import { Card, CardContent, CircularProgress, SvgIcon } from "@mui/joy";
 import Typography from "@mui/joy/Typography";
+import Divider from "@mui/joy/Divider";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import Tooltip from "@mui/joy/Tooltip";
+import Image from "next/image";
+import { dm_sans } from "@/app/utils/fonts";
+import Box from "@mui/joy/Box";
+import { dark } from "@mui/material/styles/createPalette";
 
 export default function CardDashboard() {
-  return (
-    <Card variant="outlined" invertedColors>
-      <CardContent orientation="horizontal">
-        <CircularProgress size="lg" determinate value={20}>
-          <SvgIcon>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
-              />
-            </svg>
-          </SvgIcon>
-        </CircularProgress>
-        <CardContent>
-          <Typography level="body-md">Gross profit</Typography>
-          <Typography level="h2">$ 432.6M</Typography>
-        </CardContent>
-      </CardContent>
-    </Card>
-  );
+	return (
+		<>
+			<Card
+				sx={{
+					p: 0,
+				}}
+				variant="outlined"
+				invertedColors
+			>
+				<CardContent
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "space-between",
+						height: "100%",
+					}}
+					orientation="horizontal"
+				>
+					<CardContent
+						sx={{
+							p: 2,
+							py: 0.7,
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+							gap: 1,
+						}}
+					>
+						<Typography
+							sx={{
+								fontSize: "1rem",
+								fontWeight: "500",
+								color: "text.primary",
+							}}
+							variant="h6"
+						>
+							Penerima BLT
+						</Typography>
+						<Tooltip
+							title="Jumlah Penerima Bantuan Langsung Tunai"
+							variant="solid"
+							placement="top"
+						>
+							<InfoOutlinedIcon
+								sx={{
+									fontSize: "1.1rem",
+									cursor: "pointer",
+								}}
+							/>
+						</Tooltip>
+					</CardContent>
+					<Divider orientation="horizontal" />
+					<CardContent
+						sx={{
+							p: 2,
+							display: "flex",
+							flexDirection: "row",
+							gap: 2,
+							justifyContent: "center",
+							alignItems: "center",
+						}}
+					>
+						<Box
+							sx={{
+								borderRadius: 100,
+								border: "1px solid",
+								borderColor: "divider",
+
+								p: 1,
+								width: 80,
+								height: 80,
+								display: "flex",
+								justifyContent: "center",
+								alignItems: "center",
+							}}
+						>
+							<Image
+								src="/dashboard/poverty.png"
+								width={40}
+								height={40}
+								alt="Picture of the author"
+							/>
+						</Box>
+						<Typography
+							sx={{
+								fontSize: "4rem",
+								fontWeight: "500",
+								color: "text.primary",
+								fontFamily: dm_sans.fontFamily,
+							}}
+							variant="h6"
+						>
+							<span className={dm_sans.className}>96</span>
+						</Typography>
+					</CardContent>
+				</CardContent>
+			</Card>
+			<Card
+				sx={{
+					p: 0,
+				}}
+				variant="outlined"
+				invertedColors
+			>
+				<CardContent
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "space-between",
+						height: "100%",
+					}}
+					orientation="horizontal"
+				>
+					<CardContent
+						sx={{
+							p: 2,
+							py: 0.7,
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+							gap: 1,
+						}}
+					>
+						<Typography
+							sx={{
+								fontSize: "1rem",
+								fontWeight: "500",
+								color: "text.primary",
+							}}
+							variant="h6"
+						>
+							Penyandang Disabilitas
+						</Typography>
+						<Tooltip
+							title="Jumlah Penyandang Disabilitas"
+							variant="solid"
+							placement="top"
+						>
+							<InfoOutlinedIcon
+								sx={{
+									fontSize: "1.1rem",
+									cursor: "pointer",
+								}}
+							/>
+						</Tooltip>
+					</CardContent>
+					<Divider orientation="horizontal" />
+					<CardContent
+						sx={{
+							p: 2,
+							display: "flex",
+							flexDirection: "row",
+							gap: 2,
+							justifyContent: "center",
+							alignItems: "center",
+						}}
+					>
+						<Box
+							sx={{
+								borderRadius: 100,
+								border: "1px solid",
+								borderColor: "divider",
+								backgroundColor: "#d6eaff 0.8",
+								p: 1,
+								width: 80,
+								height: 80,
+								display: "flex",
+								justifyContent: "center",
+								alignItems: "center",
+							}}
+						>
+							<Image
+								src="/dashboard/disability.png"
+								width={40}
+								height={40}
+								alt="Picture of the author"
+							/>
+						</Box>
+						<Typography
+							sx={{
+								fontSize: "4rem",
+								fontWeight: "500",
+								color: "text.primary",
+							}}
+							variant="h6"
+						>
+							<span className={dm_sans.className}>63</span>
+						</Typography>
+					</CardContent>
+				</CardContent>
+			</Card>
+		</>
+	);
 }
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,34 +1,318 @@
+"use client";
 import * as React from "react";
 import Breadcrumbs from "@/app/components/BreadCrumbs";
 import Box from "@mui/joy/Box";
 import CardDashboard from "@/app/components/Card";
+import { Card, CardContent, CircularProgress, SvgIcon } from "@mui/joy";
+import Typography from "@mui/joy/Typography";
+import Divider from "@mui/joy/Divider";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import Tooltip from "@mui/joy/Tooltip";
+import Image from "next/image";
+import { dm_sans } from "@/app/utils/fonts";
+import { ResponsiveBar } from "@nivo/bar";
+
+const data = [
+	{
+		country: "AD",
+		"hot dog": 124,
+		"hot dogColor": "hsl(6, 70%, 50%)",
+		burger: 21,
+		burgerColor: "hsl(290, 70%, 50%)",
+		sandwich: 41,
+		sandwichColor: "hsl(64, 70%, 50%)",
+		kebab: 176,
+		kebabColor: "hsl(201, 70%, 50%)",
+		fries: 180,
+		friesColor: "hsl(235, 70%, 50%)",
+		donut: 104,
+		donutColor: "hsl(190, 70%, 50%)",
+	},
+	{
+		country: "AE",
+		"hot dog": 187,
+		"hot dogColor": "hsl(33, 70%, 50%)",
+		burger: 56,
+		burgerColor: "hsl(266, 70%, 50%)",
+		sandwich: 83,
+		sandwichColor: "hsl(131, 70%, 50%)",
+		kebab: 167,
+		kebabColor: "hsl(349, 70%, 50%)",
+		fries: 154,
+		friesColor: "hsl(29, 70%, 50%)",
+		donut: 82,
+		donutColor: "hsl(157, 70%, 50%)",
+	},
+	{
+		country: "AF",
+		"hot dog": 98,
+		"hot dogColor": "hsl(58, 70%, 50%)",
+		burger: 61,
+		burgerColor: "hsl(157, 70%, 50%)",
+		sandwich: 34,
+		sandwichColor: "hsl(222, 70%, 50%)",
+		kebab: 92,
+		kebabColor: "hsl(18, 70%, 50%)",
+		fries: 189,
+		friesColor: "hsl(110, 70%, 50%)",
+		donut: 178,
+		donutColor: "hsl(116, 70%, 50%)",
+	},
+	{
+		country: "AG",
+		"hot dog": 82,
+		"hot dogColor": "hsl(311, 70%, 50%)",
+		burger: 170,
+		burgerColor: "hsl(228, 70%, 50%)",
+		sandwich: 160,
+		sandwichColor: "hsl(310, 70%, 50%)",
+		kebab: 85,
+		kebabColor: "hsl(141, 70%, 50%)",
+		fries: 48,
+		friesColor: "hsl(228, 70%, 50%)",
+		donut: 112,
+		donutColor: "hsl(359, 70%, 50%)",
+	},
+	{
+		country: "AI",
+		"hot dog": 86,
+		"hot dogColor": "hsl(315, 70%, 50%)",
+		burger: 27,
+		burgerColor: "hsl(281, 70%, 50%)",
+		sandwich: 115,
+		sandwichColor: "hsl(293, 70%, 50%)",
+		kebab: 3,
+		kebabColor: "hsl(114, 70%, 50%)",
+		fries: 50,
+		friesColor: "hsl(176, 70%, 50%)",
+		donut: 157,
+		donutColor: "hsl(224, 70%, 50%)",
+	},
+	{
+		country: "AL",
+		"hot dog": 124,
+		"hot dogColor": "hsl(18, 70%, 50%)",
+		burger: 143,
+		burgerColor: "hsl(36, 70%, 50%)",
+		sandwich: 0,
+		sandwichColor: "hsl(324, 70%, 50%)",
+		kebab: 63,
+		kebabColor: "hsl(18, 70%, 50%)",
+		fries: 67,
+		friesColor: "hsl(21, 70%, 50%)",
+		donut: 30,
+		donutColor: "hsl(219, 70%, 50%)",
+	},
+	{
+		country: "AM",
+		"hot dog": 20,
+		"hot dogColor": "hsl(38, 70%, 50%)",
+		burger: 149,
+		burgerColor: "hsl(146, 70%, 50%)",
+		sandwich: 138,
+		sandwichColor: "hsl(241, 70%, 50%)",
+		kebab: 168,
+		kebabColor: "hsl(207, 70%, 50%)",
+		fries: 164,
+		friesColor: "hsl(284, 70%, 50%)",
+		donut: 81,
+		donutColor: "hsl(111, 70%, 50%)",
+	},
+];
 
 export default function Home() {
-  return (
-    <>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <Breadcrumbs
-          items={[
-            {
-              label: "Dashboard",
-              href: "/",
-            },
-          ]}
-        />
-      </Box>
-      <Box
-        sx={{
-          width: "100%",
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill, minmax(240px, 1fr))",
-          gap: 2,
-        }}
-      >
-        <CardDashboard />
-        <CardDashboard />
-        <CardDashboard />
-        <CardDashboard />
-      </Box>
-    </>
-  );
+	return (
+		<>
+			<Box sx={{ display: "flex", alignItems: "center" }}>
+				<Breadcrumbs
+					items={[
+						{
+							label: "Dashboard",
+							href: "/",
+						},
+					]}
+				/>
+			</Box>
+			<Box
+				sx={{
+					width: "100%",
+					display: "grid",
+					gridTemplateColumns: "repeat(auto-fill, minmax(240px, 1fr))",
+					gap: 2,
+				}}
+			>
+				<CardDashboard />
+			</Box>
+			<Box
+				sx={{
+					my: 2,
+				}}
+			>
+				<Card
+					sx={{
+						height: 500,
+						width: "100%",
+						p: 0,
+						pb: 8,
+					}}
+					variant="outlined"
+					invertedColors
+				>
+					<CardContent
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "space-between",
+							height: "100%",
+						}}
+						orientation="horizontal"
+					>
+						<CardContent
+							sx={{
+								p: 2,
+								py: 0.7,
+								display: "flex",
+								flexDirection: "row",
+								alignItems: "center",
+								gap: 1,
+							}}
+						>
+							<Typography
+								sx={{
+									fontSize: "1rem",
+									fontWeight: "500",
+									color: "text.primary",
+								}}
+								variant="h6"
+							>
+								Ragam Penyandang Disabilitas
+							</Typography>
+							<Tooltip
+								title="Jumlah Penyandang Disabilitas Menurut Jenisnya"
+								variant="solid"
+								placement="top"
+							>
+								<InfoOutlinedIcon
+									sx={{
+										fontSize: "1.1rem",
+										cursor: "pointer",
+									}}
+								/>
+							</Tooltip>
+						</CardContent>
+						<Divider orientation="horizontal" />
+
+						<ResponsiveBar
+							data={data}
+							keys={[
+								"hot dog",
+								"burger",
+								"sandwich",
+								"kebab",
+								"fries",
+								"donut",
+							]}
+							indexBy="country"
+							margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+							padding={0.3}
+							valueScale={{ type: "linear" }}
+							indexScale={{ type: "band", round: true }}
+							colors={{ scheme: "nivo" }}
+							defs={[
+								{
+									id: "dots",
+									type: "patternDots",
+									background: "inherit",
+									color: "#38bcb2",
+									size: 4,
+									padding: 1,
+									stagger: true,
+								},
+								{
+									id: "lines",
+									type: "patternLines",
+									background: "inherit",
+									color: "#eed312",
+									rotation: -45,
+									lineWidth: 6,
+									spacing: 10,
+								},
+							]}
+							fill={[
+								{
+									match: {
+										id: "fries",
+									},
+									id: "dots",
+								},
+								{
+									match: {
+										id: "sandwich",
+									},
+									id: "lines",
+								},
+							]}
+							borderColor={{
+								from: "color",
+								modifiers: [["darker", 1.6]],
+							}}
+							axisTop={null}
+							axisRight={null}
+							axisBottom={{
+								tickSize: 5,
+								tickPadding: 5,
+								tickRotation: 0,
+								legend: "country",
+								legendPosition: "middle",
+								legendOffset: 32,
+							}}
+							axisLeft={{
+								tickSize: 5,
+								tickPadding: 5,
+								tickRotation: 0,
+								legend: "food",
+								legendPosition: "middle",
+								legendOffset: -40,
+							}}
+							labelSkipWidth={12}
+							labelSkipHeight={12}
+							labelTextColor={{
+								from: "color",
+								modifiers: [["darker", 1.6]],
+							}}
+							legends={[
+								{
+									dataFrom: "keys",
+									anchor: "bottom-right",
+									direction: "column",
+									justify: false,
+									translateX: 120,
+									translateY: 0,
+									itemsSpacing: 2,
+									itemWidth: 100,
+									itemHeight: 20,
+									itemDirection: "left-to-right",
+									itemOpacity: 0.85,
+									symbolSize: 20,
+									effects: [
+										{
+											on: "hover",
+											style: {
+												itemOpacity: 1,
+											},
+										},
+									],
+								},
+							]}
+							role="application"
+							ariaLabel="Nivo bar chart demo"
+							barAriaLabel={(e) =>
+								e.id + ": " + e.formattedValue + " in country: " + e.indexValue
+							}
+						/>
+					</CardContent>
+				</Card>
+			</Box>
+		</>
+	);
 }
