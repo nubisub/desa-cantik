6dfbe380f30d7478674f511cb63414427a4c7d94
nubisub <hendraksm18@mail.ugm.ac.id>
2023-09-23 21:35:52 +0700

    rm middlewar for api

--- src/app/dashboard/atur-pengguna/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Atur Pengguna | Desa Cantik - Margoagung",
-  description: "Atur Pengguna - Desa Cantik Margoagung Kabupaten Sleman",
-};
-
-export default function RootLayout({ children }) {
-  return <>{children}</>;
-}
