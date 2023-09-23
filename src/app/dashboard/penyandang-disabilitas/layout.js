130055c4b80a657ad71bd540e3bdb8b293e200b5
nubisub <hendraksm18@mail.ugm.ac.id>
2023-09-23 20:51:21 +0700

    rm page fetch

--- src/app/dashboard/penyandang-disabilitas/layout.js
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-  title: "Penyandang Disabilitas | Desa Cantik - Margoagung",
-  description:
-    "Penyandang Disabilitas - Desa Cantik Margoagung Kabupaten Sleman",
-};
-
-export default function RootLayout({ children }) {
-  return <>{children}</>;
-}
