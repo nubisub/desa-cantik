8a905e5dd647d3e0118aa4b3b90cef594815eb38
nubisub <hendraksm18@mail.ugm.ac.id>
2023-09-23 21:37:43 +0700

    patge

---
+++ src/app/dashboard/atur-pengguna/Wrapper.jsx
@@ -0,0 +1,17 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+const Page = ({ children }) => {
+  const { data, status } = useSession();
+  const router = useRouter();
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+  if (data.user.role !== "Admin") {
+    router.push("/dashboard");
+  } else {
+    return <>{children}</>;
+  }
+};
+export default Page;
---
+++ src/app/dashboard/program-keluarga-harapan/page.jsx
@@ -0,0 +1,53 @@
+// "use client";
+import * as React from "react";
+import Box from "@mui/joy/Box";
+import Breadcrumbs from "/src/app/components/BreadCrumbs";
+import Typography from "@mui/joy/Typography";
+import Table from "@/app/dashboard/program-keluarga-harapan/Table";
+// export const dynamic = "force-dynamic";
+export const revalidate = 1;
+//
+async function getData() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/bantuan/data`);
+  return res.json();
+}
+
+export default async function ProgramKeluargaHarapan() {
+  const data = await getData();
+  // const [open, setOpen] = React.useState(false);
+  return (
+    <>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Breadcrumbs
+          items={[
+            {
+              label: "Dashboard",
+              href: "/dashboard",
+            },
+            {
+              label: "Program Keluarga Harapan",
+              href: "/program-keluarga-harapan",
+            },
+          ]}
+        />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          my: 1,
+          gap: 1,
+          flexDirection: { xs: "column", sm: "row" },
+          alignItems: { xs: "start", sm: "center" },
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography level="h2">
+          Daftar Penerima Program Keluarga Harapan
+        </Typography>
+      </Box>
+
+      <Table data={data} />
+    </>
+  );
+}
