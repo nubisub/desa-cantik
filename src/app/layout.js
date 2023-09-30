aa859c2ce88f371e0ce4cbdf30bade8d8fe65a3f
nubisub <hendraksm18@mail.ugm.ac.id>
2023-09-30 21:59:16 +0700

    try og

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,44 @@ export default function RootLayout({ children }) {
         scrollBehavior: "smooth",
       }}
     >
+      <head>
+        <meta
+          property="twitter:image"
+          content="https://desa-cantik.vercel.app/api/og"
+        ></meta>
+        <meta property="twitter:card" content="summary"></meta>
+        <meta
+          property="twitter:title"
+          content="Desa Cantik - Margoagung"
+        ></meta>
+        <meta
+          property="twitter:description"
+          content="Desa Cantik Margoagung Kabupaten Sleman"
+        ></meta>
+        <meta
+          property="og:image"
+          content="https://desa-cantik.vercel.app/api/og"
+        ></meta>
+        <meta property="og:title" content="Desa Cantik - Margoagung"></meta>
+        <meta
+          property="og:description"
+          content="Desa Cantik Margoagung Kabupaten Sleman"
+        />
+
+        <meta
+          property="og:image"
+          content="https://desa-cantik.vercel.app/api/og"
+        ></meta>
+        <meta property="og:title" content="Desa Cantik - Margoagung"></meta>
+        <meta
+          property="og:description"
+          content="Desa Cantik Margoagung Kabupaten Sleman"
+        />
+        <meta
+          property="og:url"
+          content="https://desa-cantik.vercel.app/"
+        ></meta>
+      </head>
       <body style={{ margin: 0 }}>
         <NextAuthProvider>
           <CssBaseline />
